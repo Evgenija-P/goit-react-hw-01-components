d2c9ab8264164356258c51b6bfc80114881ecc4b
Evgenija-P <e.a.poduzova@gmail.com>
2022-09-24 02:36:07 +0300

    all

--- src/components/StatisticTitle/StatisticTitle.js
+++ src/components/StatisticTitle/StatisticTitle.js
@@ -1,5 +1,10 @@
 import { StatisticText } from './StatisticTitle.styled';
+import PropTypes from 'prop-types';
 
 export const StatisticTitle = ({ title }) => {
   return <StatisticText>{title}</StatisticText>;
+};
+
+StatisticTitle.propTypes = {
+  items: PropTypes.string,
 };
