148d75564cfc70f2efabadc5a3ad8a64158130b5
Evgenija-P <e.a.poduzova@gmail.com>
2022-09-23 18:25:08 +0300

    v4

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,9 @@
 import user from '../user.json';
 import data from '../data.json';
+import friends from '../friends.json';
 import { Profile } from './Profile/Profile';
 import { Statistic } from './Statistic/Statistic';
+import { FriendList } from './FriendList/FriendList';
 
 export const App = () => {
   return (
@@ -14,6 +16,7 @@ export const App = () => {
         stats={user.stats}
       />
       <Statistic stats={data} title="Upload stats" />
+      <FriendList friends={friends} />
     </>
   );
 };
--- src/components/StatisticTitle/StatisticTitle.js
+++ src/components/StatisticTitle/StatisticTitle.js
@@ -1,7 +1,3 @@
-// export const StatisticTitle = ({ title }) => {
-//   return <h2 className="title">{title}</h2>;
-// };
-
 export const StatisticTitle = ({ title }) => {
   return <h2 className="title">{title}</h2>;
 };
